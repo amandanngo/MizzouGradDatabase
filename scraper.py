5b8ab4e9653a7e985e2581104503effc2cbe5216
amandanngo <amandanngo@gmail.com>
2025-02-23 01:11:13 -0600

    outlined scraper

--- scraper.py
+++ scraper.py
@@ -1,35 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-# # Making a GET request
-# r = requests.get('https://studenthonors.missouri.edu/grad-lists/2024-spring-grad/missouri/index.html')
+#main goal: create database of all misssouri mizzou grads
+    #create functions for each step and continue passing info through functions to eventually
+    #create one database entry with name, degree, grad term & year, and county 
 
-# # Parse the content
-# soup = BeautifulSoup(r.content, 'html.parser')
+#get graduation year and term from home page
 
-# # Find the container with the class
-# container = soup.find(attrs={"class": "miz-page-content__full"})
+#get county from next page
 
-# if container:
-#     # Find the first <ul> inside the container
-#     ul = container.find('ul')
-
-#     if ul:
-#         # Get all <a> elements inside the <ul>
-#         links = ul.find_all('a')
-
-#         # Print the links
-#         for link in links:
-#             print(link['href'])  # Prints the href attribute
-#     else:
-#         print("No <ul> found in the container.")
-# else:
-#     print("Container with class 'miz-page-content__full' not found.")
-
-
-county = requests.get(f'https://studenthonors.missouri.edu/grad-lists/2024-spring-grad/missouri/adair')
-
-county_soup = BeautifulSoup(county.content, 'html.parser')
-
-
-print(county_soup)
+#get name and degree
